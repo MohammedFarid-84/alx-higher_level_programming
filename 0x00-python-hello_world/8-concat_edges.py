34618b023c160b62566c22144171a85dcd16de61
Elhlawany <aboabdallahhlawany@gmil.com>
2024-01-17 23:40:37 +0200

    edit concat_edges file

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
          language that combines remarkable power with very clear syntax"
-str = str.split(); str = str[5:7] + str[12:13] + str[0:1]; str = ' '.join(str)
+str = str[39:66] + str[114:120] + str[0:6]
 print(str)
