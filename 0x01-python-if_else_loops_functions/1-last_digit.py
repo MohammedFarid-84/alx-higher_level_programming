ff9874fd225923b52f450889014c1184c0f0954c
Elhlawany <aboabdallahhlawany@gmil.com>
2024-01-21 01:45:12 +0200

    implementation of remainder numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 
-if number > 0:
+if number >= 0:
     ldigt = number % 10
 else:
     ldigt = number % 10
@@ -13,7 +13,7 @@ else:
 
 if ldigt > 5:
     print("Last digit of", number, "is", ldigt, "and is greater than 5")
-elif ldigt  == 0:
+elif ldigt == 0:
     print("Last digit of", number, "is", ldigt, "and is 0")
 else:
     print("Last digit of", number, "is", ldigt, "and is less than 6 and not 0")
