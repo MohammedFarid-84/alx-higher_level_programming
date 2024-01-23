34ef3f59fe9ce9511419098a15cc39f2935cb55d
Elhlawany <aboabdallahhlawany@gmil.com>
2024-01-23 22:28:30 +0200

    edit uppercase code

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 
+
 def uppercase(str):
-    u = list(range(ord('A'), ord('Z') + 1))
-    l = list(range(ord('a'), ord('z') + 1))
+    upr = list(range(ord('A'), ord('Z') + 1))
+    lowr = list(range(ord('a'), ord('z') + 1))
     txupr = ""
 
-    for i  in str:
+    for i in str:
         try:
-            indx = l.index(ord(i))
-        except:
+            indx = lowr.index(ord(i))
+        except ValueError:
             indx = -1
 
-        txupr += chr(u[indx]) if indx != -1 else i
+        txupr += chr(upr[indx]) if indx != -1 else i
 
     print("{}".format(txupr))
