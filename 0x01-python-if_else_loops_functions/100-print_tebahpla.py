1fb01afbd615a518f3f85e1e96842a8b561b1038
Elhlawany <aboabdallahhlawany@gmil.com>
2024-01-25 11:58:48 +0200

    print alphbet alternatively small and captail

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,8 +4,9 @@
 def print_alfa():
     x, y = 122, 89
     while x >= 98:
-        print("{0:c}{1:c}".format(x,y), end='')
+        print("{0:c}{1:c}".format(x, y), end='')
         x -= 2
         y -= 2
+
 
 print_alfa()
