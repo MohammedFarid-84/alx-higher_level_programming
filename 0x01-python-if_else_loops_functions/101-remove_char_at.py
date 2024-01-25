7d0aa8f95dcf7a4dcc6102cab72cc4b220dd5206
Elhlawany <aboabdallahhlawany@gmil.com>
2024-01-25 13:29:14 +0200

    remove charecter from string

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -9,9 +9,6 @@ def remove_char_at(str, n):
             if i != n:
                 new_str += str[i]
     else:
-    #    for i in range(len(str)):
-    #        if i != (len(str) + n):
-    #            new_str += str[i]
         return str
 
     return new_str
