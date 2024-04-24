704a106b08e41e9240ace4b694884a4888e32eda
El-Hlawany <aboabdallhhlawany@gmail.com>
2024-04-24 14:50:27 -0700

    convert to integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function conInt() {
+  const args = process.argv[2];
+  const num = parseInt(args);
+  
+  if (!isNaN(num)) {
+    console.log(num);
+  } else {
+    console.log('Not a number');
+  }
+}
+
+conInt();
