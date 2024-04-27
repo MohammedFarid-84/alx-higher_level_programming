ae93a66a904e2ae58b1d7899758115069cbdee2c
El-Hlawany <aboabdallahhlawany@gmail.com>
2024-04-27 17:32:44 +0300

    print rectangle method

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (width, height) {
+    if (width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+  print() {
+    let x = 'X';
+    if (this.width > 0 && this.height > 0) {
+      for (let i = 1; i < this.width; i++) {
+        x = x + 'X';
+      }
+      for (let i = 0; i < this.height; i++) {
+        console.log(x);
+      }
+    }
+  }
+}
+
+module.exports = Rectangle;
