b053faafabf9174baa284f76aba7eaf21ffc9721
El-Hlawany <aboabdallahhlawany@gmail.com>
2024-04-28 23:47:58 +0300

    edit format

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -4,15 +4,10 @@ const dict = require('./101-data').dict;
 const newDict = {};
 
 for (const key in dict) {
-    const value = dict[key];
-    if (!newDict[value]) {
-        newDict[value] = [];
-    }
-    if (newDict[value]) {
-        newDict[value].push(key.toString());
-    } else {
-        newDict[value] = key.toString();
-    }
+  const value = dict[key];
+  if (!newDict[value]) { newDict[value] = []; }
+  if (newDict[value]) {
+    newDict[value].push(key.toString());
+  }
 }
 console.log(newDict);
-
