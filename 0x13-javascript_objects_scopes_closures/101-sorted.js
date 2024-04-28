8b2ade90af0e0ba1cf05f10383f539d7dd9e6117
El-Hlawany <aboabdallahhlawany@gmail.com>
2024-04-28 23:28:56 +0300

    convert vlaue to key in dictionary

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,5 +1,18 @@
 #!/usr/bin/node
 
 const dict = require('./101-data').dict;
+const newDict = {};
 
-console.log(dict);
+for (const key in dict) {
+    const value = dict[key];
+    if (!newDict[value]) {
+        newDict[value] = [];
+    }
+    if (newDict[value]) {
+        newDict[value].push(key.toString());
+    } else {
+        newDict[value] = key.toString();
+    }
+}
+console.log(newDict);
+
