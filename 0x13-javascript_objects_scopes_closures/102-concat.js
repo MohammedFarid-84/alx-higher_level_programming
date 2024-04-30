19928f703e563791227e7d6b34c703ce33233dec
Mohamed Farid <aboabdallahhlawany@gmail.com>
2024-04-30 22:37:13 +0300

    read and concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,10 +3,17 @@ const filesAry = process.argv[2];
 const fs = require('fs');
 
 if (filesAry !== undefined) {
-    for (let i = 2; i < 4; i++) {
-      const fileName = process.argv[i];
-      //console.log(fileName);
-      const data = fs.readFileSync(fileName, 'utf-8')
-      fs.writeFileSync(filesAry[2], data);
+  for (let i = 2; i < 4; i++) {
+    const fileName = process.argv[i];
+    const data = fs.readFileSync(fileName, 'UTF-8');
+
+    if (data.charCodeAt(0) === 0xFEFF) {
+      data = data.substr(1);
     }
+    if (i < 3) {
+      fs.writeFileSync(process.argv[4], data);
+    } else {
+      fs.appendFileSync(process.argv[4], data);
+    }
+  }
 }
