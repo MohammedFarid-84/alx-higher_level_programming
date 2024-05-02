b60ce0c62d337630faaf47bd3f6218093f4773a7
Mohamed Farid <aboabdallahhlawany@gmail.com>
2024-05-03 00:25:26 +0300

    use group by

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- count rows and show resultes in a deferent column.
-SELECT score, COUNT(score) AS 'number' FROM second_table GROUP BY score;
+SELECT score, COUNT(score) AS 'number' FROM second_table GROUP BY score ORDER BY score DESC;
