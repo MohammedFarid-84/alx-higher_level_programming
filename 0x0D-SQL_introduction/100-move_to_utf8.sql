186c93d5e397451ac3e69bb0464460f7449f727d
Mohamed Farid <aboabdallahhlawany@gmail.com>
2024-05-03 16:07:28 +0300

    edit table

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- edit table structure after built it.
 ALTER DATABASE hbtn_0c_0 DEFAULT COLLATE=utf8mb4_unicode_ci;
 USE hbtn_0c_0;
-ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
