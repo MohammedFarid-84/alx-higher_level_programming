72962082a1f8a27a2c08b319a5da5fed3c2d3e56
Mohamed Farid <aboabdallahhlawany@gmail.com>
2024-05-03 14:10:13 +0300

    select top 3 row

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- list rows in a maximum three row only (limit) do that.
-SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city, month HAVING month >= 7 AND month <= 8 ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month >= 7 AND month <= 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
