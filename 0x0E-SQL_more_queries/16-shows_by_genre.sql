36392a0795a52668e1d7548656da7280b0810b22
Mohamed Farid <aboabdallahhlawany@gmail.com>
2024-05-05 22:32:24 +0300

    show all show

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- show count of show genner.
+SELECT tv_shows.title AS title, tv_genres.name AS name
+    FROM tv_shows LEFT JOIN (tv_genres, tv_show_genres)
+      ON (tv_genres.id = tv_show_genres.genre_id AND tv_shows.id = tv_show_genres.show_id)
+    ORDER BY tv_shows.title;
