1325e016e8ec7d04d2af3d3f4f13a9aa55cd4aed
Mohamed Farid <aboabdallahhlawany@gmail.com>
2024-05-05 23:06:15 +0300

    rearrang names

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,4 +2,4 @@
 SELECT tv_shows.title AS title, tv_genres.name AS name
     FROM tv_shows LEFT JOIN (tv_genres, tv_show_genres)
       ON (tv_genres.id = tv_show_genres.genre_id AND tv_shows.id = tv_show_genres.show_id)
-    ORDER BY tv_shows.title;
+    ORDER BY tv_shows.title, tv_genres.name;
