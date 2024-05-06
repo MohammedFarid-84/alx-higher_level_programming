23d5a4abc404fbb33e8492e0e54004c6aa2f8caa
Mohamed Farid <aboabdallahhlawany@gmail.com>
2024-05-06 17:55:13 +0300

    create read only user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,5 @@
 -- create new database and new user.
 -- grnat the new user read only privilege.
-DROP DATABASE IF EXISTS hbtn_0d_2;
-CREATE DATABASE hbtn_0d_2;
-DROP USER IF EXISTS 'user_0d_2'@'localhost';
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY'user_0d_2_pwd';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
