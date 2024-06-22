18a43efb0ddfed79b55d68a1121d3968c31d4f11
Mohamed Farid <aboabdallahhlawany@gmail.com>
2024-06-22 03:01:15 +0300

    edit 1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-from add_0 import add
+import add_0
 
 
 a = 1
 b = 2
-c = add(a, b)
+c = add_0.add(a, b)
 
-
-if __name__ == "__main__":
-    print('{} + {} = {}'.format(a, b, c))
+print('{} + {} = {}'.format(a, b, c))
