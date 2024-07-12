049bedbfd2aaae838b03b7fa16b87c48337cecac
Mohamed Farid <aboabdallahhlawany@gmail.com>
2024-07-13 01:19:16 +0300

    my calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, div, mul
+from sys import argv, stderr, exit
+
+
+def doCalc(argvs):
+    opr = {'+':add, '-':sub, '*':mul, '/':div}
+    if len(argvs) < 4:
+        print(f'Usage: {argvs[0]} <a> <operator> <b>', file=stderr)
+        exit(1)
+    elif argvs[2] not in opr:
+        print('Unknown operator. Available operators: +, -, * and /', file=stderr)
+        exit(1)
+    else:
+        n1 = int(argvs[1])
+        n2 = int(argvs[3])
+        func = opr[argvs[2]]
+        result = func(n1, n2)
+        print(f'{n1} {argvs[2]} {n2} = {result}')
+    
+
+if __name__ == "__main__":
+    doCalc(argv)
+    
