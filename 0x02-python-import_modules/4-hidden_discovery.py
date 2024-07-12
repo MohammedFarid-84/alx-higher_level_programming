78340806f515929efa4617d0d6f4688ac1849a79
Mohamed Farid <aboabdallahhlawany@gmail.com>
2024-07-12 23:35:31 +0300

    import the hidden file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 import hidden_4 as libs
 
-print(dir(libs))
+
+def printList(lst):
+    for lib in lst:
+        if not lib.startswith('__'):
+            print(lib)
+
+if __name__ == "__main__":
+    myList = dir(libs)
+    printList(myList)
