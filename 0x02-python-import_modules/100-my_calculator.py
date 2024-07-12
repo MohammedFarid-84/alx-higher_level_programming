220631c1dcb457061376b20eceb1f07474439b54
Mohamed Farid <aboabdallahhlawany@gmail.com>
2024-07-13 01:29:36 +0300

    my calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, div, mul
-from sys import argv, stderr, exit
+from sys import argv, exit
 
 
 def doCalc(argvs):
-    opr = {'+':add, '-':sub, '*':mul, '/':div}
+    opr = {'+': add, '-': sub, '*': mul, '/': div}
     if len(argvs) < 4:
-        print(f'Usage: {argvs[0]} <a> <operator> <b>', file=stderr)
+        print(f'Usage: {argvs[0]} <a> <operator> <b>')
         exit(1)
     elif argvs[2] not in opr:
-        print('Unknown operator. Available operators: +, -, * and /', file=stderr)
+        print('Unknown operator. Available operators: +, -, * and /')
         exit(1)
     else:
         n1 = int(argvs[1])
@@ -17,8 +17,7 @@ def doCalc(argvs):
         func = opr[argvs[2]]
         result = func(n1, n2)
         print(f'{n1} {argvs[2]} {n2} = {result}')
-    
+
 
 if __name__ == "__main__":
     doCalc(argv)
-    
