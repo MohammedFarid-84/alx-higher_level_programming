62ca3ef75f8fe31bb1befb98d42c84d2b3eb3d40
Mohamed Farid <aboabdallahhlawany@gmail.com>
2024-07-19 22:54:01 +0300

    appent into tuple hahaha

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,8 +4,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if tuple_a and tuple_b:
         for i in range(0, 2):
             if len(tuple_a) == len(tuple_b):
-                x = tuple_a[i]
-                y = tuple_b[i]
+                x = tuple_a[i] if i < len(tuple_a) else 0
+                y = tuple_b[i] if i < len(tuple_b) else 0
             elif len(tuple_a) > len(tuple_b):
                 x = tuple_a[i]
                 y = tuple_b[i] if i < len(tuple_b) else 0
@@ -15,7 +15,9 @@ def add_tuple(tuple_a=(), tuple_b=()):
             z = x + y
             new_tuple.append(z)
     elif tuple_a:
-        new_tuple.append(tuple_a)
+        new_tuple = tuple_a
     elif tuple_b:
-        new_tuple.append(tuple_b)
+        new_tuple = tuple_b
+    else:
+        new_tuple = [0, 0]
     return tuple(new_tuple)
