a73c8cde9151c2c58cc824b73b3b5362891f170b
Mohamed Farid <aboabdallahhlawany@gmail.com>
2024-07-23 23:10:38 +0300

    test if list is a palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,29 +1,6 @@
 #include "lists.h"
 
 /**
- * revers_list - revers singl list.
- * @head: a linked list.
- * Return: a revers linked list.
- */
-listint_t *revers_list(listint_t *head)
-{
-	listint_t *current = head;
-	listint_t *next = NULL;
-	listint_t *prev = NULL;
-
-	while (current != NULL)
-	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
-
-	return (prev);
-}
-
-
-/**
  * is_palindrome - test if the duobly linked list
  * is a palindrome or not.
  * @head: a duobly linked list.
@@ -32,29 +9,40 @@ listint_t *revers_list(listint_t *head)
 int is_palindrome(listint_t **head)
 {
 	listint_t *h = *head;
-	listint_t *front = NULL;
-	listint_t *rev_list = NULL;
+	int *my_arry;
+	int s = 0, i = 0;
 
-	/* save old list in new list */
+	/* count nodes in the list */
 	while (h != NULL)
 	{
-		add_nodeint_end(&front, h->n);
+		s++;
 		h = h->next;
 	}
 	h = *head;
 
-	/* reverse the old list */
-	rev_list = revers_list(h);
+	/* intilize the array */
+	my_arry = malloc(s * sizeof(int));
 
-	/* commpare the list from to sides */
-	while (front != NULL && rev_list != NULL)
+	/* save the list in the array */
+	while (h != NULL)
 	{
-		if (front->n != rev_list->n)
+		my_arry[i] = h->n;
+		h = h->next;
+		i++;
+	}
+	h = *head;
+	i = 0;
+	/* commpare values in the array and free array */
+	while (h != NULL)
+	{
+		if (my_arry[i] != my_arry[s - 1 - i])
 		{
+			free(my_arry);
 			return (0);
 		}
-		front = front->next;
-		rev_list = rev_list->next;
+		h = h->next;
+		i++;
 	}
+	free(my_arry);
 	return (1);
 }
