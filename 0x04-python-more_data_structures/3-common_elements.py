1f274b0c2be3a20bd22f693cc4a28c618c7b0d91
Mohamed Farid <aboabdallahhlawany@gmail.com>
2024-09-07 15:45:30 +0300

    get common elements

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,8 @@
 #!/usr/bin/pyhton3
 
 def common_elements(set_1, set_2):
-    lst = []
+    lst = set()
     for x in set_1:
         if x in set_2 and x not in lst:
-            lst.append(x)
+            lst.add(x)
     return lst
